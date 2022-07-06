5bcae016ff3569efb7d9a5f8d2bed149baf0e9ef
mrizkyhaksono <mrizkyhaksono@gmail.com>
2022-07-06 22:00:09 +0700

    first commit

---
+++ src/com/rizky/Main.java
@@ -0,0 +1,24 @@
+package com.rizky;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("2D Adventure");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        gamePanel.startGameThread();
+    }
+}
